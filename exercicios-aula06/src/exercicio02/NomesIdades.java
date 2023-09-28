0e591db46fa227502478c4f073b2a33a4ef70319
lincklopescarol <85366877+lincklopescarol@users.noreply.github.com>
2023-09-27 22:08:03 -0300

    primeira parte do exercício 02 da aula 06

---
+++ exercicios-aula06/src/exercicio02/NomesIdades.java
@@ -0,0 +1,33 @@
+package exercicio02;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class NomesIdades {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Map<String, Integer> nomesIdades = new HashMap<>();
+        nomesIdades.put("Carol", 19);
+        nomesIdades.put("Vitória", 20);
+        nomesIdades.put("Júlia", 65);
+        nomesIdades.put("Roberta", 60);
+
+        System.out.println("Digite um nome: ");
+        String respostaUsuario = teclado.nextLine();
+
+        if (nomesIdades.containsKey(respostaUsuario)) {
+            System.out.println("Nome encontrado.");
+            System.out.println("A idade da pessoa com esse nome é: ");
+            System.out.println(nomesIdades.get(respostaUsuario));
+        } else {
+            System.out.println("Nome não encontrado.");
+        }
+
+    }
+
+
+}
+
+
+
